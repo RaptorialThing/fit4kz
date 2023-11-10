f2c4688da7fbbdbc5d0e5667e5891c8661d61219
RaptorialThing <ruzal.gabdrahmanoff@inbox.lv>
2023-11-10 13:25:13 +0300

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 AdminUser.create!(email: 'admin@example.com', password: 'password') if Rails.env.development?
+AdminUser.create!(email: 'nomad@nomad.com', password: 'password') if Rails.env.development?
 Setting.create_or_find_by!(key: 'min_version', value: '0.0')
